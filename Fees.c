a20d07a02565f28d52330eca5dee3decb26daab7
Owen Lovett <ollovett@cpp.edu>
2022-11-27 19:44:41 -0800

    Update Fees.c

--- Fees.c
+++ Fees.c
@@ -35,3 +35,53 @@ int validateTime(int time){
         return -1;
     }
 }
+
+double privateCar(double miles){
+    double price;
+
+    price = 0.27 * miles;
+
+    calculateExpenses(price, price, 1); //flat fee, so allowedExpenses is same as fee
+    return price;
+}
+
+double rentalCar(int days, double pricePerDay){
+    double price;
+
+    price = pricePerDay * days;
+
+    calculateExpenses(price, price, 1); //flat fee, so allowedExpenses is same as fee
+    return price;
+}
+
+double parkingFees(int days, double fee){
+    double price;
+
+    price = fee * days;
+
+    calculateExpenses(price, 6, days);
+    return price;
+}
+
+double taxiFees(int days, double fee){
+    double price;
+
+    price = fee * days;
+
+    calculateExpenses(price, 10, days);
+    return price;
+}
+
+double registrationFees(double fee){
+    calculateExpenses(fee, fee, 1); //flat fee, so allowedExpenses is same as fee
+    return fee;
+}
+
+double hotelFees(int days, double fee){
+    double price;
+
+    price = fee * days;
+
+    calculateExpenses(price, 90, days);
+    return price;
+}

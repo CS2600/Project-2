18e2a8e3839d71691a08912a102d5863be354104
henry <htat@cpp.edu>
2022-11-28 19:16:25 -0800

    created header file

--- FeesCalculator.h
+++ FeesCalculator.h
@@ -1,54 +1,18 @@
-#include "Fees.h"
-
 double allowedExpenses;
 double static totalExpenses;
 double static totalAllowableExpenses;
 double static excessExpenses;
 double static amountSaved;
 
-//Calculates allowed expenses
-void calculateExpenses(double fee, double dailyAllowedExpenses, int numberOfDays){
-    allowedExpenses = dailyAllowedExpenses * numberOfDays;
-    totalAllowableExpenses += allowedExpenses;;
- 
-//Calculates total expenses
-    totalExpenses +=fee;
-    
-    if(fee-allowedExpenses > 0)
-    {
-        //Calculates excess expenses
-        excessExpenses += (fee - allowedExpenses);
-    }
+void calculateExpenses(double fee, double dailyAllowedExpenses, int numberOfDays);
 
-    if(fee-allowedExpenses < 0)
-    {
-        //Calculates amount saved
-        amountSaved += (allowedExpenses - fee);
-    }
-}
+void setTotalExpenses(double fee);
+void setTotalAllowableExpenses(double fee);
+void setExcessExpenses(double fee);
+void setAmountSaved(double fee);
 
-void setTotalExpenses(double fee){
-    totalExpenses = fee;
-}
-void setTotalAllowableExpenses(double fee){
-    totalAllowableExpenses = fee;
-}
-void setExcessExpenses(double fee){
-    excessExpenses = fee;
-}
-void setAmountSaved(double fee){
-    amountSaved = fee;
-}
+double getTotalExpenses();
+double getTotalAllowableExpenses();
+double getExcessExpenses();
+double getAmountSaved();
 
-double getTotalExpenses(){
-    return totalExpenses;
-}
-double getTotalAllowableExpenses(){
-    return totalAllowableExpenses;
-}
-double getExcessExpenses(){
-    return excessExpenses;
-}
-double getAmountSaved(){
-    return amountSaved;
-}   

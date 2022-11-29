d59fb707f2d3c9cfb76b0d175c5fbe234ff78571
Owen Lovett <ollovett@cpp.edu>
2022-11-28 19:43:42 -0800

    added header file

---
+++ Fees.h
@@ -0,0 +1,13 @@
+int validateFee(double fee);
+int validateMiles(double miles);
+int validateDays(int days);
+int validateTime(int time);
+
+int privateCar(double miles);
+int rentalCar(int days, double pricePerDay);
+int parkingFees(int days, double fee);
+int taxiFees(int days, double fee);
+int registrationFees(double fee);
+int hotelFees(int days, double fee);
+int eligibleMeals(int depart, int arrival);
+int mealFees(int departMeal, int arriveMeal, double departFee, double arriveFee);
